1babf142dcc31fe5131ef2b44d1001ac2e45356e
pasan wedikkara <pasan.madusanka9@gmail.com>
2022-12-11 21:06:37 +0900

    action update

--- app/src/main/java/com/example/navigationdrawersampletest/MainActivity.kt
+++ app/src/main/java/com/example/navigationdrawersampletest/MainActivity.kt
@@ -2,10 +2,44 @@ package com.example.navigationdrawersampletest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
+    private  lateinit var  drawerLayout: DrawerLayout
+    private  lateinit var navController: NavController
+    private lateinit var navView:NavigationView
+
+    private lateinit var  appBarConfiguration: AppBarConfiguration
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        drawerLayout = findViewById(R.id.drawer_layout)
+        navController = findNavController(R.id.nav_host)
+        navView = findViewById(R.id.navView)
+
+    // appBarConfiguration = AppBarConfiguration(navController.graph,drawerLayout)
+        appBarConfiguration = AppBarConfiguration(
+           setOf(R.id.nav_home,R.id.nav_setting,R.id.nav_help),drawerLayout
+       )
+
+
+        setupActionBarWithNavController(navController,drawerLayout)
+        navView.setupWithNavController(navController)
+
+
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp(appBarConfiguration)||super.onSupportNavigateUp()
     }
 }
